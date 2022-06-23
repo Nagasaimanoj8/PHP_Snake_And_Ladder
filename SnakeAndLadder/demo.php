5d824b69a0864377abffc00b9d058a38f8064481
unknown <nagasaimanoj333@gmail.com>
2022-06-23 08:31:24 +0530

    [Manoj]Adding_Random

--- SnakeAndLadder/demo.php
+++ SnakeAndLadder/demo.php
@@ -5,9 +5,18 @@ class snake{
  private $previousposition;
  public function welcome(){
    echo "Welcome to Snake And Ladder Game \n";
-   echo "Any Player starts from $this->startposition";
+   echo "Any Player starts from $this->startposition"."\n";
+ }
+ public function play()
+{
+    $previousposition=$this->startposition;
+    $this->startposition=rand(1, 6);
+    echo" the next position after dice rolled is:$this->startposition"."\n";
+    return $this->startposition;
+    $this->nextMove($this->startposition);
  }
 }
 $obj=new snake();
 $obj->welcome();
+$obj->play();
 ?>
