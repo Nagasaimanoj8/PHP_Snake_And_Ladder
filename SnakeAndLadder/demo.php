2a9df98e296ac4b2c321cbd7b0f5e67ea7a7ef37
unknown <nagasaimanoj333@gmail.com>
2022-06-24 22:50:21 +0530

    [Manoj]Adding_Exact

--- SnakeAndLadder/demo.php
+++ SnakeAndLadder/demo.php
@@ -1,7 +1,8 @@
 <?php
 class snake{
  private $count=0;
- private $startposition=0;
+ public$startposition=0;
+ private $rolldice;
  private $previousposition;
  public function welcome(){
    echo "Welcome to Snake And Ladder Game \n";
@@ -11,51 +12,62 @@ class snake{
 {
     $previousposition=$this->startposition;
     $this->count++;
-    $this->startposition=rand(1, 6);
-     //  echo" the next position after dice rolled is :$this->startposition"."\n";
+    $this->rolldice=rand(1, 6);
+    //  echo" the next position after dice rolled is :$this->startposition"."\n";
     return $this->startposition;
-    $this->nextMove($this->startposition);
+    
  }
  public function option(){ 
-  
-   return rand(1,3);
- 
+     return rand(1,3);
 }
  //create function to check option for NO Play,Ladder,Snake
  public function nextMove($option)
  {
-   while($this->startingposition!=100){
-
    
-      switch ($option) {
-         case 1:          
-            
-            echo " No Play " . $this->previousposition = $this->startpositionposition;
-            echo "\n";
-             break;
-         case 2:
-            echo " player on Ladder move to next position " ;
-            $this->position += $this->startposition;
-          echo "\n";
-               break;
-         case 3:
-            echo "player taker the snake";  
-            $this->previousposition -= $this->startposition;
-            echo "\n";
-            echo "\n";
-              break; 
-         }  
-         echo "Current Position is:" . $this->previousposition . "\n";
-            
-          
-   }
-}
+               
+
+              $this->previousposition = $this->startposition;
+              switch ($option) {
+ 
+             case 1:
+                 $this->startposition = $this->startposition;
+                 echo "Previous Position : $this->previousposition & StartPosition :  $this->startposition \n";
+                 break;
+
+             case 2:
+                
+                 $this->startposition += $this->rolldice;
+                 if($this->startposition < 100){
+                     echo "Previous Position : $this->previousposition & StartPosition : $this->startposition \n";
+                 }
+
+                 elseif($this->startposition > 100){
+                     $this->startposition = $this->previousposition;
+                     echo "Previous Position : $this->previousposition & StartPosition :  $this->startposition  \n";
+                 }
+                 elseif($this->startposition == 100){
+                     echo "Previous Position : $this->previousposition & StartPosition :  $this->startposition \n";
+                     echo "Player won the game";
+                 }
+                 break;
+
+             case 3:
+                 $this->startposition -= $this->rolldice;
+                 if($this->startposition < 0){
+                     $this->startposition = 0;
+                 }
+                 echo "Previous Position : $this->previousposition & StartPosition :  $this->startposition . \n";
+                 break;
+         }
+        
+      }
 }
 
-$obj=new snake();
-$obj->welcome();      
-//$obj->play();
+$obj=new snake();//object create
+$obj->welcome();//call function
+while($obj->startposition!=100 ){
+$obj->play();
 $option =$obj->option();
 $obj->nextMove($option);
-
+}
 ?>
